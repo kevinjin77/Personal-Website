f7a3b9161b79b64f4ce04e4728af2f6a0c5a1f10
Kevin Jin <kevinjin77@gmail.com>
2017-06-26 23:07:35 -0400

    Update main.js
    Image-Info functionality, moved textillate script from HTML to JS.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,27 @@
-/* Javascript Code */
+$(document).ready(function() {
+    // This command is used to initialize some elements and make them work properly
+    $.material.init();
+});
+
+$('.tlt').textillate({
+    loop: true,
+    in : {
+        effect: 'fadeIn',
+        delayScale: 1.5,
+        delay: 30,
+    },
+    out: {
+        effect: 'fadeOut',
+        delayScale: 1.5,
+        delay: 30,
+        shuffle: true,
+    },
+});
+
+$("span.glyphicon.glyphicon-info-sign").mouseover(function() {
+    this.parentNode.nextSibling.nextSibling.style.display = "inline-block";
+});
+
+$("span.glyphicon.glyphicon-info-sign").mouseout(function() {
+    this.parentNode.nextSibling.nextSibling.style.display = "none";
+});
